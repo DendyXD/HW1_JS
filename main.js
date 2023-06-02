8b75def92236dd84e13923fc44f940f8f60a0b68
Vanya <i.boychyshyn@gmail.com>
2023-06-02 16:08:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+// Age
+
+const age = 12;
+
+console.log("age:>>", age)
+
+// Name
+
+const name = "Ivan";
+
+console.log("name:>>", name)
+
+// isStudent
+
+const isStudent = true;
+
+console.log("Is student:>>", isStudent)
+
+// myString
+
+const myString = "вабалєба";
+
+console.log("My string:>>", myString)
+
+// myNumber
+
+const myNumber = -10;
+
+console.log("My number + 10:>>", myNumber + 10)
+
+// muNull
+
+const myNull = null;
+
+console.log("My null:>>", myNull)
+
+// User name
+
+const userName = prompt("Введіть ім'я користувача");
+
+console.log("Вітаємо,", userName)
+
+// Alert + Confirm
+
+alert("Дія небезпечна")
+
+const confirmed = confirm("Всерівно підтвердити дію?");
+
+confirmed ? alert('Дякую за підтвердження') : alert('Дію відмінено');
